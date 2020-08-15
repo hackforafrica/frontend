3680b21df143775e9e3734fdf75442b6de9a294a
ANNMAINAWANGARI <50842222+ANNMAINAWANGARI@users.noreply.github.com>
2020-08-15 15:27:11 +0300

    registry fetch api

--- src/Login.js
+++ src/Login.js
@@ -8,6 +8,16 @@ import Input from '@material-ui/core/Input';
 
 
 function Login() {
+  const loginFunction = async()=>{await fetch("http://hackforafrica.herokuapp.com/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({'emailaddress':'ann@gmail.com','passw':'123456789'})
+})
+.then((response)=>response.json())
+.then((data)=>{
+  console.log(data)});}
+    
+  
  return (
    <div className='LoginStyle'>
     <div className='Login__page'>
@@ -28,7 +38,7 @@ function Login() {
        />
        <p className='info'>To keep up the pace login with your personal info</p>
        <Link to='/Main'>
-       <button className='login__button'type='submit'>LOG IN</button>
+       <button className='login__button'type='submit' onClick={loginFunction}>LOG IN</button>
        </Link>
        <p className='sign_up'>Don't have an account</p>
        <Link to='/Register' className='click__signup'>CLICK HERE
--- src/Register.js
+++ src/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState}from 'react';
 import './Register.css';
 import {Link} from 'react-router-dom';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
@@ -6,13 +6,27 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Input from '@material-ui/core/Input';
-
 function Register() {
+const [user_info,setuser_Info] = useState([{passw:'',fullname:'',emailaddress:''}]);
+  const registerFunction = (event)=>{
+     event.preventDefault();
+     fetch("https://hackforafrica.herokuapp.com/register",{
+       method:"POST",
+       headers:{"Content-Type":"application/json"},
+       body:JSON.stringify({user_info})
+     })
+     .then((res)=>res.json())
+     .then((data)=>console.log('yeeeeeeeeey', data))
+     .catch((err)=>console.log('noooooo' ,err))
+     
+     
+  }
+ 
  return (
 <div className='registerStyle'>
   <div className='register__page'>
    <h1>CREATE ACCOUNT</h1>
-   <Input className='name' placeholder='name' type='text'disableUnderline='true'
+   <Input className='name' placeholder='name' value={user_info.fullname}type='text'disableUnderline='true'onChange={event=>setuser_Info(event.target.value)}
      startAdornment={
       <InputAdornment position="start">
         <AccountCircleIcon/>
@@ -20,14 +34,14 @@ function Register() {
      }
    />
    <p >or use your email for registration</p>  
-   <Input className='mail' placeholder='email' type='email'disableUnderline='true'
+   <Input className='mail' placeholder='email' value={user_info.emailaddress}type='email'disableUnderline='true'onChange={event=>setuser_Info(event.target.value)}
      startAdornment={
       <InputAdornment position="start">
         <MailOutlineIcon/>
       </InputAdornment>
      }
    />
-   <Input className='pass' placeholder='password' type='password'disableUnderline='true'
+   <Input className='pass' placeholder='password' value={user_info.passw}type='password'disableUnderline='true'onChange={event=>setuser_Info(event.target.value)}
      startAdornment={
       <InputAdornment position="start">
         <VisibilityOffIcon/>
@@ -35,7 +49,7 @@ function Register() {
      }
    />
    <Link to='/Main'>
-    <button className='signup-button'>SIGN UP</button>
+    <button className='signup-button' onClick={registerFunction}>SIGN UP</button>
    </Link>
   </div>
   </div>
